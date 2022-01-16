e7a49005b470bb8f441276882e906d9241cdeece
Utkarsh Vishwakarma <utkarshrkt2001@gmail.com>
2022-01-16 22:37:22 +0530

    ✨ Feat : created Server and connected to MongoDb cluster

---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const app = express();
+
+const connectDB = require("./config/db");
+
+const dotenv = require("dotenv");
+
+dotenv.config();
+connectDB();
+
+app.use(express.json());
+
+
+
+app.get("/", (req, res) => {
+  res.send("Api is running ");
+});
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, console.log(`Server started on port ${PORT} 🚀`));
