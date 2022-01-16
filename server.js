867720aeaf7c68749d0bf36efad833d04c21592d
Utkarsh Vishwakarma <utkarshrkt2001@gmail.com>
2022-01-16 22:41:43 +0530

    ✨ feat : created middlewares for handling errors

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const connectDB = require("./config/db");
 
 const dotenv = require("dotenv");
 const userRoutes = require("./routes/userRoutes");
+const { notFound, errorHandler } = require("./middlewares/errorMiddlerware");
 
 dotenv.config();
 connectDB();
@@ -16,6 +17,10 @@ app.use("/fyd/api/users", userRoutes);
 app.get("/", (req, res) => {
   res.send("Api is running ");
 });
+
+app.use(notFound);
+app.use(errorHandler);
+
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, console.log(`Server started on port ${PORT} 🚀`));
